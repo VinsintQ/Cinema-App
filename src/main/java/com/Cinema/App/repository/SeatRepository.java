8343845233c617b8519aee996d964d794809efc0
mueen ali <hejiy2002@gmail.com>
2026-04-02 21:21:27 +0300

    adding seat repo

--- src/main/java/com/Cinema/App/repository/SeatRepository.java
+++ src/main/java/com/Cinema/App/repository/SeatRepository.java
@@ -1,13 +1,21 @@
 package com.Cinema.App.repository;
 
 import com.Cinema.App.model.Seat;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SeatRepository extends JpaRepository<Seat, Long> {
     List<Seat> findByHallId(Long hallId);
     void deleteByHallId(Long hallId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT s FROM Seat s WHERE s.id = :id")
+    Optional<Seat> findByIdWithLock(Long id);
 }
